a54131ff4f6d6871d3ea37cbd281d714607b91eb
Somia Saeed <somiasaeedpk@gmail.com>
2020-08-31 12:17:27 +0500

    bmi calculator file added

---
+++ cpp/bmi-calculator.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+int main(){
+    std::cout<<"name of person.\n";
+    std::cout<<"age of person.\n";
+    std::cout<<"height of person.\n";
+    std::cout<<"weight of person.\n";
+    return 0;
+}
