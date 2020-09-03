7bd75b973cd8bd73d1d05b19f41c0a3a3f9377d2
Somia Saeed <somiasaeedpk@gmail.com>
2020-09-03 13:19:04 +0500

    data types practice file added

---
+++ cpp/data-types-practice.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a =4;
+    int b = 5;
+    cout<<"Here the value of a is "<<a<<". The value of b is "<< b;
+    return 0;
+}
+
+
---
+++ cpp/variable-comments-practice.cpp
@@ -0,0 +1,2 @@
+// by the way you can also run your program from top right 2nd button
+// ok carry on ur code etc
