b799b8b4673e0cfe1f5dafe122a6d40b5cfa6eb1
Somia Saeed <somiasaeedpk@gmail.com>
2020-09-05 01:25:55 +0500

    booleean data type added

--- cpp/data-types-practice.cpp
+++ cpp/data-types-practice.cpp
@@ -8,9 +8,11 @@ int main(){
     int a=4, b=5;
     char d ='b';
     float pi = 3.14;
+    bool overweight = true;
     cout<<"Here the value of a is "<<a<<". \nThe value of b is "<< b;
-    cout<<"\nThe value of a is: "<<a;
+    cout<<"\nThe value of d is: "<<d;
     cout<<"\nthe value of pi is: "<<pi;
+    cout<<"\nthe value of overweight is: "<<overweight;
     return 0;
 }
 
