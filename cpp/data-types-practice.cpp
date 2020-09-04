9c498cc252895212c880496c00c554cf348fe5a6
Somia Saeed <somiasaeedpk@gmail.com>
2020-09-04 12:02:13 +0500

    modified data types practice file

--- cpp/data-types-practice.cpp
+++ cpp/data-types-practice.cpp
@@ -3,9 +3,14 @@
 using namespace std;
 
 int main(){
-    int a =4;
-    int b = 5;
-    cout<<"Here the value of a is "<<a<<". The value of b is "<< b;
+    // int a = 4;
+    // int b = 5;
+    int a=4, b=5;
+    char d ='b';
+    float pi = 3.14;
+    cout<<"Here the value of a is "<<a<<". \nThe value of b is "<< b;
+    cout<<"\nThe value of a is: "<<a;
+    cout<<"\nthe value of pi is: "<<pi;
     return 0;
 }
 
