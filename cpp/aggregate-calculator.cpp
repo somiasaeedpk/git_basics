cdf3c990cdf6df4aaab6020fdfaa890079578c75
Somia Saeed <somiasaeedpk@gmail.com>
2020-09-13 11:22:34 +0500

    add new changes

--- cpp/aggregate-calculator.cpp
+++ cpp/aggregate-calculator.cpp
@@ -11,15 +11,15 @@ int main(){
     cin >> name;
     cout<<"enter total marks in matric:\n";
     cin >> total_matric;
-    cout<<"enter matric obtained marks:\n";// here total marks in matric are 1050
+    cout<<"enter matric obtained marks:\n";
     cin >> matric;
     cout<<"enter total marks in fsc:\n";
     cin >> total_fsc;
-    cout<<"enter fsc obtained marks:\n";//here total marks in fsc are 1100
+    cout<<"enter fsc obtained marks:\n";
     cin >> fsc;
     cout<<"enter total marks in ecat:\n";
     cin >> total_ecat;
-    cout<<"enter obtained marks in ecat:\n";//here total marks in ecat are 400
+    cout<<"enter obtained marks in ecat:\n";
     cin >> ecat;
     double aggregate =matric/total_matric * 25 + fsc/total_fsc * 45 + ecat/total_ecat * 30 ; 
     cout << "aggregate= " <<aggregate;
