8fe17c27b4923b6afefb36e9423b5ece7c96348d
Somia Saeed <somiasaeedpk@gmail.com>
2020-10-20 00:25:32 +0500

    area of circle

---
+++ cpp/circle_area.cpp
@@ -0,0 +1,13 @@
+//write program to calculate radius of circle
+#include<iostream>
+using namespace std;
+
+int main(){
+	float radius, area;
+	
+	cout<<"radius"<<endl;
+	cin>>radius;
+	area = 4*3.14*(radius*radius);
+	cout<<"area= "<<area;
+	return 0;
+}
